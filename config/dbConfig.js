12a4fbcb74cd623f38d1f6e3f63527e011284611
Samar Ranjit <samarranjit321@gmail.com>
2024-11-06 23:45:36 -0600

    fix mongouri

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,10 @@
-// dbConfig.js
 const mongoose = require('mongoose');
 require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(`${MONGO_URL}`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 })
-.then(() => console.log('connected to mongo db'))
-.catch((err) => console.log(err));
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.log('MongoDB connection error:', err));
