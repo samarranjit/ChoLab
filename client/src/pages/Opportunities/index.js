bd120a02ab56a7914bc961f2e7d6006900473a32
Samar Ranjit <samarranjit321@gmail.com>
2024-11-26 19:51:26 -0600

    [Update] : Opportunities Page Changes

--- client/src/pages/Opportunities/index.js
+++ client/src/pages/Opportunities/index.js
@@ -25,7 +25,7 @@ function Opportunities() {
 
           </div>
           <p className='leading-relaxed sm:text-justify'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In sequi assumenda at aperiam recusandae consequuntur nam soluta repudiandae vel, doloribus ipsa quos natus rerum dignissimos magnam obcaecati veritatis? Accusantium odio hic cumque aperiam ea quidem mollitia ut provident doloremque commodi enim ipsam dolorum itaque atque, saepe excepturi quas iste tenetur.
+          We are building a dynamic team of undergraduate and graduate students, as well as postdoctoral researchers, who share a passion for advancing our understanding of hydrology and water resources in a changing climate to promote a sustainable environment. We value self-motivation, a collaborative spirit, and a commitment to collective success. Our lab fosters an inclusive and diverse environment where everyone is empowered to thrive and contribute meaningfully.
           </p>
         </div>
       </div>
@@ -35,19 +35,19 @@ function Opportunities() {
         <div className="grid grid-cols-3 text-center gap-5 sm:grid-cols-1">
           <div className="skillsList my-7">
             <p className='text-tertiary text-lg'>01</p>
-            <h2 className="text-2xl p-2 ">Mathematical Modeling</h2>
-            <p className='p-5 leading-loose'>Strong quantitative foundation (e.g. degree in math, physics, or computer science) and an interest in applying computational engineering to water resources.</p>
+            <h2 className="text-2xl p-2 ">Hydrology and Climate</h2>
+            <p className='p-5 leading-loose'> Investigating hydrological processes under changing climate conditions using land surface and climate models.</p>
 
           </div>
           <div className="skillsList my-7">
             <p className='text-tertiary text-lg'>02</p>
-            <h2 className="text-2xl p-2 ">Policy Analysis</h2>
-            <p className='p-5 leading-loose'> Training and experience in policy analysis or environmental justice, with in interest in bridging social science and engineering.</p>
+            <h2 className="text-2xl p-2 ">Remote Sensing and Data Analytics</h2>
+            <p className='p-5 leading-loose'>Applying satellite/UAV sensing and AI/machine learning to predict water systems, crop productivity, and extreme events.</p>
           </div>
           <div className="skillsList my-7">
             <p className='text-tertiary text-lg'>03</p>
-            <h2 className="text-2xl p-2 ">Water Resources</h2>
-            <p className='p-5 leading-loose'>Expertise in hydrology and water resources engineering and interest in developing strong computational skills.</p>
+            <h2 className="text-2xl p-2 ">Sustainable Water Management</h2>
+            <p className='p-5 leading-loose'> Developing solutions for drought, flooding, and water quality challenges to support resilient communities and environmental justice.</p>
           </div>
 
         </div>
