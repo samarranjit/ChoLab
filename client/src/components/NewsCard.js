99a99ae07a50e36096b9e87ea19dfbb8be34ec2c
Samar Ranjit <samarranjit321@gmail.com>
2025-06-30 11:59:05 -0500

    feat: integrate ReactMarkdown in NewsCard for improved body rendering and enhance layout in Research and About pages

--- client/src/components/NewsCard.js
+++ client/src/components/NewsCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 function NewsCard(props) {
   const { _id, heading, body, mainImage, date } = props.prop;
@@ -21,7 +22,10 @@ function NewsCard(props) {
         <h3 className="p-5 text-2xl text-secondary font-semibold sm:text-xl md:px-0 md:py-2">{heading}</h3>
         <p className='px-5 py-4 pt-2 text-gray-400 text-left md:p-0'>Date: {date}</p>
         <p className="px-5 justify-center md:px-0 md:py-2">
-          {body[0].length > 150 ? body[0].substring(0, 150) + "..." : body[0]}
+          <ReactMarkdown>
+
+            {body[0].length > 150 ? body[0].substring(0, 150) + "..." : body[0]}
+          </ReactMarkdown>
         </p>
         <button className="m-5 border-tertiary border-[2px] px-5 py-4 rounded-[10px] w-[70%] sm:w-[50%] sm:p-2 sm:my-5 flex items-center mx-auto justify-center text-center font-semibold bg-tertiary text-primary text-xl sm:text-sm border-b-[5px] hover:text-tertiary hover:bg-primary hover:border-b-[5px] transition duration-500"
           onClick={() => handleReadMore()}
--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -60,16 +60,26 @@ function About() {
       <DrCho />
       <OtherMember />
       <div className="">
-        <div style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/StaticImages/Opportunities.jpg)` }} className=" h-[90vh] overflow-hidden sm:h-[80vh] bg-fixed  bg-cover bg-bottom -mb-[5.5rem]">
-          <div className=' w-[100%] h-[100%] bg-opacity-15 flex items-center justify-center  sm:ml-0 sm:p-2 sm:justify-center '>
-            <div className="collaboration-call w-[40%] sm:w-[90%] md:w-[70%] p-10 gap-7  flex flex-col items-center justify-center bg-secondary bg-opacity-65 sm:gap-2 sm:p-4 ">
-              <p className=' text-2xl text-tertiary text-center w-full font-semibold align-center justify-center sm:text-lg'>Do you want to join the team?</p>
-              <h1 className=' text-3xl md:text-2xl sm:text-xl font-semibold text-primary  '>Join the Cho Lab</h1>
-              <p className='text-primary '>We are building a dynamic team of undergraduate and graduate students and postdocs who share our passion to better understand hydrology and water resources in a changing climate for a sustainable environment. To achieve this, we use field observations from station data and field campaign, multiple remote sensing techniques, land surface hydrologic models, and climate models along with big-data and machine learning (ML) techniques.</p>
-              <a href="/opportunities" target='_blank' rel='noreferrer'>
-
-                <button className='border-primary border-2 p-5 text-primary px-7 hover:bg-primary hover:text-black duration-500'>Lab Opportunities</button>
-              </a>
+        <div
+          style={{ backgroundImage: `url(/StaticImages/Opportunities.jpg)` }}
+          className="h-[80vh] md:h-[90vh] overflow-hidden bg-fixed bg-cover bg-bottom -mb-[5.5rem]"
+        >
+          <div className='w-[100%] h-[100%] bg-opacity-15 flex items-center justify-center ml-0 p-2 md:ml-0 md:p-2'>
+            <div className="collaboration-call w-[90%] md:w-[70%] lg:w-[40%] p-4 gap-2 md:gap-7 md:p-10 flex flex-col items-center justify-center bg-secondary bg-opacity-65">
+              <p className='text-lg md:text-2xl text-tertiary text-center w-full font-semibold align-center justify-center'>
+                Do you want to join the team?
+              </p>
+              <h1 className='text-xl md:text-2xl lg:text-3xl font-semibold text-primary'>
+                Join the Cho Lab
+              </h1>
+              <p className='text-primary'>
+                We are building a dynamic team of undergraduate and graduate students and postdocs who share our passion to better understand hydrology and water resources in a changing climate for a sustainable environment. To achieve this, we use field observations from station data and field campaign, multiple remote sensing techniques, land surface hydrologic models, and climate models along with big-data and machine learning (ML) techniques.
+              </p>
+              <Link href="/opportunities">
+                <button className='border-primary border-2 p-5 text-primary px-7 hover:bg-primary hover:text-black duration-500'>
+                  Lab Opportunities
+                </button>
+              </Link>
             </div>
           </div>
         </div>
