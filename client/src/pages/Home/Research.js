5d7618102a6eaab785ba81acbaa1327897baf227
Samar Ranjit <samarranjit321@gmail.com>
2025-07-06 21:22:06 -0500

    fix: adjust layout of Research component for better responsiveness

--- client/src/pages/Home/Research.js
+++ client/src/pages/Home/Research.js
@@ -9,7 +9,7 @@ function Research() {
 
   return (
     <div className='h-[90vh] px-[50px] py-[50px]  flex justify-center items-center sm:flex-col md:justify-around md:h-full md:p-7 dark:bg-slate-800 max-w-[1775px] mx-auto'>
-      <div className="left w-[100%] md:w-[55%] p-5 md:p-1 mx-auto ">
+      <div className="left w-[55%] md:w-[100%] p-5 md:p-1  ">
         <p className='text-tertiary text-2xl sm:text-xl '>About Our Research</p>
 
         <div className="right w-[50%] -z-2 sm:w-[100%] hidden sm:inline sm:my-1 ">
@@ -35,8 +35,7 @@ function Research() {
           <button className='my-10 p-5 border-secondary border-[2px] rounded-[50px] text-secondary font-semibold text-1xl hover:text-primary hover:bg-secondary transition duration-500' >Our Research</button>
         </Link>
       </div>
-      <div className="right w-[100%] -z-2 md:w-[30%] sm:hidden pb-[4rem]">
-
+      <div className="right w-[30%] -z-2 md:w-[75%] sm:w-[100%] sm:hidden pb-[4rem]">
         {/* <lottie-player src="https://lottie.host/33499152-b2e8-4197-bacb-a7c3022dc1b3/8J5ZDjrToX.json"  background="transparent"  speed="1"  style={{width:" 100%", height: "100%"}} loop className='animators sm:w-[100%]' autoplay></lottie-player> */}
         <img alt='' src={`${process.env.PUBLIC_URL}/StaticImages/Home_ResearchBgImage.jpg`} className='p-2 -translate-y-5' />
 
